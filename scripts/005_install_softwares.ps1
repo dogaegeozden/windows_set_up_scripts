c652eb9703699bcdd2080f74a5e8624cc8c2277b
Doga <61098483+dogaegeozden@users.noreply.github.com>
2022-09-16 17:14:23 -0400

    Update 005_install_softwares.ps1

--- scripts/005_install_softwares.ps1
+++ scripts/005_install_softwares.ps1
@@ -1,7 +1,7 @@
 # Update the chocolatey package manager.
 choco upgrade chocolatey
 # An array for list of softwares
-$softwares = @("atom", "androidstudio", "flameshot", "protonvpn", "vlc", "obs-studio", "brave", "blender", "shotcut", "inkscape", "krita", "libreoffice-fresh", "okular", "gimp", "xmind", "veracrypt", "virtualbox", "adobereader", "python3", "wsl-ubuntu-2204", "everything")
+$softwares = @("atom", "freerdp", "androidstudio", "flameshot", "protonvpn", "vlc", "obs-studio", "brave", "blender", "shotcut", "inkscape", "krita", "libreoffice-fresh", "okular", "gimp", "xmind", "veracrypt", "virtualbox", "adobereader", "python3", "wsl-ubuntu-2204", "everything")
 
 # Loop through the array to install each one of these softwares.
 For ($s=0; $s -lt $softwares.Length; $s++){
